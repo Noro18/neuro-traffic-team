fd9ef0877a2d393bfc8788b292537df4c1050411
InoLopes <angelinorosaleslopes1234@gmail.com>
2025-06-27 08:47:22 +0900

    hadia link dashboard ,aumenta login

--- monitorApp/urls.py
+++ monitorApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 path('', views.monitorApp, name="app"),
-path('dashboard',  views.monitorApp, name="dashboard"),
-path('inference/', views.video_feed, name="inference"),
-path('analysis/', views.analysis, name="analysis"),
+path('dashboard',  login_required(views.dashboard), name="dashboard"),
+path('inference/', login_required(views.video_feed), name="inference"),
+path('analysis/', login_required(views.analysis), name="analysis"),
 ]
