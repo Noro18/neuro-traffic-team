ba3f177f1db8213a6864d190a5f8d212e7eb9daa
Noro18 <zeroxg534@gmail.com>
2025-06-26 14:38:41 +0900

    registu database ba admin

--- monitorApp/admin.py
+++ monitorApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Detecta, Class, DetailDetail
+admin.site.register(Detecta)
+admin.site.register(Class)
+admin.site.register(DetailDetail)
